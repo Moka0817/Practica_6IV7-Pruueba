72a2cb9935f2d6a2c3c492a77fb971b2260a6224
Moka0817 <61607902+Moka0817@users.noreply.github.com>
2020-02-28 20:26:54 -0600

    Update Hola.js

--- Hola.js
+++ Hola.js
@@ -1,5 +1,4 @@
 const http = require('http');
-
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -11,4 +10,4 @@ const server = http.createServer((req,res)=>{
 
 server.listen(port, hostname, ()=>{
 	console.log('Server running at http://${hostname}:${port}/');
-})
+});
